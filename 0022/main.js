e1f496d6f443c06cc3c7b7a161ddbcf2f68ceaa6
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-03-27 14:02:33 -0400

    Add files via upload

---
+++ 0022/main.js
@@ -0,0 +1,25 @@
+var person_1 = {
+					first_name: "Bob",
+					last_name: "The Great",
+					age: 22
+			   };
+			   
+var person_2 = {
+					first_name: "Robin",
+					last_name: "The Hood",
+					age: 33
+			   };
+			   
+var output;   
+
+output = "";
+output = output + person_1["first_name"] + "<br>";
+output = output + person_1["last_name"] + "<br>";
+output = output + person_1["age"] + "<br>";
+document.getElementById("p1").innerHTML = output;
+
+output = "";
+output = output + person_2["first_name"] + "<br>";
+output = output + person_2["last_name"] + "<br>";
+output = output + person_2["age"] + "<br>";
+document.getElementById("p2").innerHTML = output;
---
+++ 0023/main.js
@@ -0,0 +1,32 @@
+var person_1 = {
+					first_name: "Bob",
+					last_name: "The Great",
+					age: 22
+			   };
+			   
+var person_2 = {
+					first_name: "Robin",
+					last_name: "The Hood",
+					age: 33
+			   };
+			   
+var output;   
+var property;
+
+output = "";
+for(property in person_1)  
+{
+	output = output + person_1[property] + "<br>";
+}
+document.getElementById("p1").innerHTML = output;
+
+//perperty = first_name, property = last_name, property = age
+
+output = "";
+for(property in person_2)
+{
+	output = output + person_2[property] + "<br>"; 
+}
+document.getElementById("p2").innerHTML = output;
+
+//perperty = first_name, property = last_name, property = age
