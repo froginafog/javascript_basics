0305480fae0f2da31dbe15c25e83e38f631af5c7
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-04-05 13:50:15 -0400

    Update main.js

--- 0021/main.js
+++ 0021/main.js
@@ -1,14 +1,14 @@
 var person_1 = {
-					first_name: "Bob",
-					last_name: "The Great",
-					age: 22
-			   };
+				first_name: "Bob",
+				last_name: "The Great",
+				age: 22
+	       };
 			   
 var person_2 = {
-					first_name: "Robin",
-					last_name: "The Hood",
-					age: 33
-			   };
+				first_name: "Robin",
+				last_name: "The Hood",
+				age: 33
+	       };
 			   
 var output;   
 
