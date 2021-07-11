a93a95d9df64d25642879e62e258cb8214a29cae
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-07-10 23:17:05 -0400

    Delete main.js

--- 0001/main.js
+++
@@ -1,8 +0,0 @@
-//What comes after double slashes is a comment.
-/*What comes between slash-star and star-slash is also a comment.*/
-//To run this program, open the "index.html" file with a web browser.
-//Comments are not executed. They are only here to state what the code does.
-
-document.body.getElementsByTagName("h1").innerHTML = "Hello Frog";  //To print "Hello Frog" on the webpage on the <h1> tag.
-document.body.getElementsByTagName("p").innerHTML = "Bye Frog";     //To print "Bye Frog" on the webpage on the <p> tag.
-
