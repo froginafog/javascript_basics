8a29eefa14c54d740fb2628654926a5bd1ef5ac9
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-07-10 23:17:15 -0400

    Add files via upload

---
+++ 0001/main.js
@@ -0,0 +1,8 @@
+//What comes after double slashes is a comment.
+/*What comes between slash-star and star-slash is also a comment.*/
+//To run this program, open the "index.html" file with a web browser.
+//Comments are not executed. They are only here to state what the code does.
+
+document.body.getElementsByTagName("h1")[0].innerHTML = "Hello Frog";  //To print "Hello Frog" on the webpage on the <h1> tag.
+document.body.getElementsByTagName("p")[0].innerHTML = "Bye Frog";     //To print "Bye Frog" on the webpage on the <p> tag.
+
