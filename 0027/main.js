cf2d35efb4dc885b5c2a706b0b025f9a39d0876a
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-25 21:23:41 -0400

    Add files via upload

---
+++ 0027/main.js
@@ -0,0 +1,21 @@
+print_message_to_paragraph_1 = () =>
+{
+    document.getElementById("p1").innerHTML = "Hello Frog";
+}
+
+print_message_to_paragraph_2 = () =>
+{
+    document.getElementById("p2").innerHTML = "Bye Frog";
+}
+
+function main()
+{
+    print_message_to_paragraph_1();
+    print_message_to_paragraph_2();
+}
+
+main();
+
+/*
+To JS or not to JS.
+*/
